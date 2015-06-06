ece6259d8c6ab2078893ec398db2946477ba25b7
mambax7 <mambax7@gmail.com>
2015-06-06 07:58:38 -0400

    Original files

---
+++ ixt01715_v30/xo-info.php
@@ -0,0 +1,39 @@
+<?php
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01715
+* @theme_release 3.0
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01715
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: xo-info.php 2779 2010-04-25 08:36:55Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+return array(
+ "languages"   => array("main"),
+ "copyright"   => "&copy; IXThemes Project <http://ixthemes.org>",
+ "name"        => "ixt01715",
+ "description" => "XOOPS 2.0.1x, 2.3.x, 2.4.x and 2.5.x, fixed width 1000px, 3 columns, preheader, slides, ext.header, ext.footer, 2 control zones, concatenation of left and right columns, presets for visibility of roll-up blocks",
+ "version"     => "3.0",
+ "format"      => "XOOPS",
+ "author"      => "IXThemes Project, support@ixthemes.org",
+ "demo"        => "http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01715",
+ "demo1"       => "http://xoops233demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo2"       => "http://xoops240demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo3"       => "http://xoops241demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo4"       => "http://xoops242demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo5"       => "http://xoops243demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo6"       => "http://xoops244demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo7"       => "http://xoops245demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "demo8"       => "http://xoops250demo.ixthemes.org/index.php?xoops_theme_select=ixt01715",
+ "url"         => "http://ixthemes.org",
+ "download"    => "http://ixthemes.com",
+ "W3C"         => "XHTML 1.0 Transitional - CSS 1/2/3",
+ "licence"     => "GPL",
+ "thumbnail"   => "shot.gif",
+ "screenshot"  => "screenshot.jpg",
+);
+?>
